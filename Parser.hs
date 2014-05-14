a16c337a229dcbc05e12e82ce4c5baf13848ab40
Jeremy Leakakos <jleakakos@cyrusinnovation.com>
2014-05-14 12:46:23 -0400

    Inline delimiter functions

--- Parser.hs
+++ Parser.hs
@@ -1,30 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Parser where 
 
-import Person
+import Person (makeGender, makeDay, dateDashFormat, dateSlashFormat, Person(..))
 import Data.List.Split (splitOn)
 import qualified Data.Text as T
 
-pipeDelim :: T.Text
-pipeDelim = "|"
-
 makePipePerson :: T.Text -> Person
 makePipePerson line = Person fn ln (makeGender g) c (makeDay dateDashFormat dob)
- where (ln:fn:_:g:c:dob:[]) = splitLine pipeDelim line
-
-commaDelim :: T.Text
-commaDelim = ","
+ where (ln:fn:_:g:c:dob:[]) = splitLine "|" line
 
 makeCommaPerson :: T.Text -> Person
 makeCommaPerson line = Person fn ln (makeGender g) c (makeDay dateSlashFormat dob)
- where (ln:fn:g:c:dob:[]) = splitLine commaDelim line
-
-spaceDelim :: T.Text
-spaceDelim = " "
+ where (ln:fn:g:c:dob:[]) = splitLine "," line
 
 makeSpacePerson :: T.Text -> Person
 makeSpacePerson line = Person fn ln (makeGender g) c (makeDay dateDashFormat dob)
- where (ln:fn:_:g:dob:c:[]) = splitLine spaceDelim line
+ where (ln:fn:_:g:dob:c:[]) = splitLine " " line
+
+detectDelimAndSplitSlint :: T.Text -> [T.Text]
+detectDelimAndSplitSlint = undefined
 
 splitLine :: T.Text -> T.Text -> [T.Text]
 splitLine delim line = map T.strip $ T.splitOn delim line
