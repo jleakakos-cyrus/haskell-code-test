c9e06ad663c0d07f6a7b615a8ab688ad27d69dab
Jeremy Leakakos <jleakakos@cyrusinnovation.com>
2014-05-15 11:57:54 -0400

    Cleanup of attoparsec implementation

--- APParser.hs
+++ APParser.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 module APParser where
 import Prelude hiding (takeWhile)
-
+import System.IO.Unsafe
 import Person
 
-import Data.Char (isSpace)
+import Data.Char (isAlpha, isSpace)
 import Data.Attoparsec.Text
 import Data.Text (strip, Text)
 import Control.Applicative
@@ -12,12 +12,13 @@ import qualified Data.Text.IO as TIO
 
 main = do
   commaContent <- TIO.readFile "data/comma.txt"
-  let Right commaPeople = parseOnly (parsePeople ',') commaContent
+  let Right commaPeople = parseOnly parseCommaPeople commaContent
   pipeContent <- TIO.readFile "data/pipe.txt"
-  let Right pipePeople = parseOnly (parsePeople '|') pipeContent
+  let Right pipePeople = parseOnly parsePipePeople pipeContent
   spaceContent <- TIO.readFile "data/space.txt"
-  let Right spacePeople = parseOnly (parsePeople ' ') spaceContent
+  let Right spacePeople = parseOnly parseSpacePeople spaceContent
   let people = pipePeople ++ commaPeople ++ spacePeople
+
   putStrLn "Output 1:"
   mapM_ print $ sortByGenderAndLastNameAscending people
   putStrLn ""
@@ -28,34 +29,33 @@ main = do
   mapM_ print $ sortByLastNameDescending people
   putStrLn ""
 
-parsePeople :: Char -> Parser [Person]
-parsePeople delim 
-  | delim == ',' = many' $ (parseCommaPerson) <* (endOfLine <|> endOfInput)
-  | delim == '|' = many' $ (parsePipePerson) <* (endOfLine <|> endOfInput)
-  | delim == ' ' = many' $ (parseSpacePerson) <* (endOfLine <|> endOfInput)
+parseCommaPeople :: Parser [Person]
+parseCommaPeople = many' $ (parseCommaPerson) <* (endOfLine <|> endOfInput)
+
+parsePipePeople :: Parser [Person]
+parsePipePeople = many' $ (parsePipePerson) <* (endOfLine <|> endOfInput)
+
+parseSpacePeople :: Parser [Person]
+parseSpacePeople = manyTill (parseSpacePerson <* endOfLine) endOfInput
 
 parseSpacePerson :: Parser Person
 parseSpacePerson =
-  let isContent c = (not $ isSpace c)
-  in do
+   do
+    ln <- takeWhile isAlpha
     skipSpace
-    ln <- takeWhile isContent
-    skipDelim ' '
+    fn <- takeWhile isAlpha
     skipSpace
-    fn <- takeWhile isContent
-    skipDelim ' '
+    mi <- takeWhile isAlpha
     skipSpace
-    mi <- takeWhile isContent
-    skipDelim ' '
+    g <- takeWhile isAlpha
     skipSpace
-    g <- takeWhile isContent
-    skipDelim ' '
+    dob <- takeTill isSpace
     skipSpace
-    dob <- takeWhile isContent
-    skipDelim ' '
-    skipSpace
-    fc <- takeWhile isContent
+    fc <- takeWhile isAlpha
     return $ Person fn ln (makeGender g) fc (makeDay dateDashFormat dob)
+
+skipDelim :: Char -> Parser ()
+skipDelim delim = skipSpace >> char delim >> skipSpace
 
 parsePipePerson :: Parser Person
 parsePipePerson =
@@ -63,25 +63,15 @@ parsePipePerson =
   in do
     skipSpace
     ln <- takeWhile isContent
-    skipSpace
     skipDelim '|'
-    skipSpace
     fn <- takeWhile isContent
-    skipSpace
     skipDelim '|'
-    skipSpace
     mi <- takeWhile isContent
-    skipSpace
     skipDelim '|'
-    skipSpace
     g <- takeWhile isContent
-    skipSpace
     skipDelim '|'
-    skipSpace
     fc <- takeWhile isContent
-    skipSpace
     skipDelim '|'
-    skipSpace
     dob <- takeWhile isContent
     return $ Person fn ln (makeGender g) fc (makeDay dateDashFormat dob)
 
@@ -91,25 +81,12 @@ parseCommaPerson =
   in do
     skipSpace
     ln <- takeWhile isContent
-    skipSpace
     skipDelim ','
-    skipSpace
     fn <- takeWhile isContent
-    skipSpace
     skipDelim ','
-    skipSpace
     g <- takeWhile isContent
-    skipSpace
     skipDelim ','
-    skipSpace
     fc <- takeWhile isContent
-    skipSpace
     skipDelim ','
-    skipSpace
     dob <- takeWhile isContent
     return $ Person fn ln (makeGender g) fc (makeDay dateSlashFormat dob)
-
-skipDelim :: Char -> Parser ()
-skipDelim delim = skip $ (== delim)
-
-
